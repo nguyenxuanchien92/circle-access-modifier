5c6394b5f4a1184607fdffc0bc39a4116aada4b3
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-11-10 15:50:28 +0700

    done for access modifier

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import models.Circle;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(2.0,"blue");
+
+        System.out.println(circle.getColor());
+    }
+}
